5482f0166c915f663a6dd78235a645ac0b67f7ce
Shepherd Xie <shepherd.xie@gmail.com>
2022-11-16 21:42:41 +0800

    build: add logback

--- src/main/java/com/orkva/utils/easy/excel/ExcelReader.java
+++ src/main/java/com/orkva/utils/easy/excel/ExcelReader.java
@@ -1,5 +1,9 @@
 package com.orkva.utils.easy.excel;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
 import java.util.List;
 
 /**
@@ -9,6 +13,7 @@ import java.util.List;
  * @version 2022/11/15
  */
 public class ExcelReader {
+    private static final Logger logger = LoggerFactory.getLogger(ExcelReader.class);
 
     /**
      * Read target path excel file to T class list instance.
@@ -19,7 +24,13 @@ public class ExcelReader {
      * @return parsed data list
      */
     public static <T> List<T> read(String path, Class<T> clazz) {
+        logger.debug("Read file path: {}", path);
+        File sourceFile = new File(path);
         return null;
+    }
+
+    public static void main(String[] args) {
+        read("", null);
     }
 
 }
